5e19daa406a8fb88287c7790d2081530a3295598
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-06-04 17:48:11 -0400

    Single Page Application with Vue.js 2.0 1.0  /default.vue

---
+++ default.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("default-component", {
+      template: template // the variable template will be injected
+    });
+  });
+</script>
