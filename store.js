2e9c94e11847be072cd7c8b661b86a1ddd1829b8
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-06-29 17:39:04 -0400

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -8,7 +8,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
       url: null
     },
     actions: {
-      LOAD_MALL_DATA: function (state, {commit}) {
+      LOAD_MALL_DATA: function ({commit, state}) {
         return new Promise((resolve, reject) => {
           axios.get(state.url).then(response => {
             commit('SET_MALL_DATA', { list: response.data })
