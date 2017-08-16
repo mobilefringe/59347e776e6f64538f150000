a3f6cd6e68de6a8065a928ddbe924043cb57dad9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-16 10:33:20 -0400

    MM SDK with Vue.js 2.0 1.0  /mallmaverick_svgmap.js

--- mallmaverick_svgmap.js
+++ mallmaverick_svgmap.js
@@ -412,7 +412,7 @@
             mark.width = this.width;
             mark.height = this.height;
             var something = _this.markAddFinalStep(mark, create);
-            console.log("marksAdd 2");
+            console.log("marksAdd 2", something);
             return something
           };
 
