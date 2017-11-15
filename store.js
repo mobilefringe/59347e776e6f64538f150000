e94e7d192c5a067755a0fe9e9155cb396255aafd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 17:06:47 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -137,8 +137,8 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
       },
       findPromoById: (state, getters) => (id) => {
         let promos = getters.processedPromos;
-        console.log(promos)
-        console.log(promos.find(promo => promo.id === id))
+        // console.log(promos)
+        // console.log(promos.find(promo => promo.id === id))
         return promos.find(promo => _.toNumber(promo.id) === id)
       },
       findMetaDataByPath: (state, getters) => (path) => {
