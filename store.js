25bed2ff2a8f39d2fd8681d78cf92504ed9e6ee4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-04 13:55:40 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -226,7 +226,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
             return blogs.find(blog => blog.slug === slug )
         },
         findBlogPostBySlug : (state, getters) => (name, slug) => {
-            let blogs = state.results.blogs(name);
+            let blogs = findBlogByName(name);
             console.log(blogs)
             return blogs.find(blog => blogs.posts.slug === slug )
         },
