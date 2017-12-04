fe0bacd1e812e54602f9a2c8b1685c286176e59d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 17:40:34 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -56,8 +56,8 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
         },
     },
     mutations: {
-        SET_MALL_DATA: (state, {list, id}) => {
-            state.results.id = list;
+        SET_MALL_DATA: (state, {list, data_id}) => {
+            state.results[data_id] = list;
         },
         SET_LOCALE: (state, { lang }) => {
             state.locale = lang
