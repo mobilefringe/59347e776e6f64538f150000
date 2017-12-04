525fbb8d1ea0b621d2d495339ee190a889bb5ff6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-04 17:14:43 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -12,7 +12,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
             return new Promise((resolve, reject) => {
                 var url = "https://www.mallmaverick.com/api/v4/" + list.url + "stores.json";
                 console.log(url);
-                axios.get(url + "stores.json").then(response => {
+                axios.get(url).then(response => {
                     commit('SET_MALL_DATA', { list: response.data, id : "stores" })
                     console.log(response.data);
                     resolve(response);
