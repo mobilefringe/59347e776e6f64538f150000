470569fba826bd7b1e1dbfbbd2f0d923e0100e0c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-06 10:44:40 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -103,7 +103,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
                 let hours = state.results.hours;
                 let property = state.results.property;
                 let timezone = property.timezone_moment;
-                let allHours = _.filter(hours, function(o) { return o.store_ids === null && o.is_holiday == 0; });
+                let allHours = _.filter(hours, function(o) { return o.store_ids === null && !o.is_holiday; });
                 // let holidayHours = hours.find(hour => hour.is_holiday == true && (moment(hour.holiday_date).tz(timezone).date() == moment().tz(timezone).date() && moment(hour.holiday_date).tz(timezone).month() + 1 == moment().tz(timezone).month() + 1 && moment(hour.holiday_date).tz(timezone).year() == moment().tz(timezone).year()));
                 console.log(hours,allHours);
                 let hoursObject = allHours;
