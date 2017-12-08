853dbbd0abe83b31b6fd799f0c969c14b4a853ad
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-08 11:48:19 -0500

    MM SDK with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -53,7 +53,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
         },
         CONTACT_US: function ({commit}, form_data) {
             return new Promise((resolve, reject) => {
-                axios.post('/api/v1/contact_us', data).then(response => {
+                axios.post('/api/v1/contact_us', form_data).then(response => {
                     resolve(response);
                 }).catch(error => {
                     console.log("Data load error: " + error.message);
