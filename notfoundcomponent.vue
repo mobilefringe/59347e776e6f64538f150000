99d602f5540d43449c1526ac441d45df080213ed
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-12-29 00:38:29 -0500

    MM SDK with Vue.js 2.0 1.0  /notfoundcomponent.vue

--- notfoundcomponent.vue
+++ notfoundcomponent.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<script type="module">
+<script type="text/babel">
   define(["Vue"], function(Vue) {
     return Vue.component("not-found-component", {
       template: template, // the variable template will be injected
