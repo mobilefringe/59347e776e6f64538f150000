74eb6f2536adfd9b1986cbc181db9897f102f56f
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-12-29 02:30:55 -0500

    MM SDK with Vue.js 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -50,7 +50,7 @@
   }
 </style>
 
-<script type="text/babel">
+<script>
   define(["Vue"], function(Vue) {
     return Vue.component("stores-component", {
       template: template, // the variable template will be injected
