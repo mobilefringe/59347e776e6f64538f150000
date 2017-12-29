63c0f4f4607fb139f39582b26c3411fb77c00d56
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-12-29 01:24:05 -0500

    MM SDK with Vue.js 2.0 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script type="text/babel">
-  define(["Vue", "vue-meta"], function(Vue, Meta) {
+  define(["Vue", "vue-meta", "babel"], function(Vue, Meta, babel) {
     Vue.use(Meta);
     return Vue.component("default-component", {
       template: template, // the variable template will be injected
