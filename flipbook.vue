aa74a7bad3090f93c7e52ccbec71576a87920d1a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-04 16:53:37 -0500

    MM SDK with Vue.js 2.0 1.0  /flipbook.vue

---
+++ flipbook.vue
@@ -0,0 +1,26 @@
+<template>
+	
+</template>
+
+<script>
+    define(["Vue", ], function(Vue, ) {
+        return Vue.component('flipbook', {
+            template: template,
+        	props: {
+,
+        	},
+        	watch: {
+
+        	},
+        	methods: {
+
+        	},
+        	mounted: function() {
+        		
+        	},
+        	destroyed: function() {
+
+        	}
+        });
+    });
+</script>
