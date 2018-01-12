d1abc8397d46f33a342d04b7c61e8d0b98bc4df0
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2018-01-12 12:59:00 -0500

    MM SDK with Vue.js 2.0 1.0  /datastore_mutations.js

--- datastore_mutations.js
+++ datastore_mutations.js
@@ -1,21 +1,25 @@
 define(['datastore_mutation_types', 'js-cookie'], function (types, Cookies) {
   const mutations = {
-    [types.SET_MALL_DATA] (state, {list}) {
-      state.results = list;
+    [types.SET_API_DETAILS] (state, {api}) {
+      state.api = api;
     },
     [types.SET_LOCALE] (state, {lang}) {
       state.locale = lang
       Cookies.set('locale', lang);
     },
+    [types.SET_MALL_DATA] (state, {list}) {
+      state.results = list;
+    },
     [types.SET_META_DATA] (state, {list}) {
       state.meta_data = list;
+    },
+    [types.SET_PROPERTY] (state, {property}) {
+      state.property = property;
     },
     [types.SET_STORES] (state, {stores}) {
       state.stores = stores;
     },
-    [types.SET_API_DETAILS] (state, {api}) {
-      state.api = api;
-    },
+    
   }
   return mutations;
 });
