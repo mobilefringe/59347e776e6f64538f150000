bc99f0dcda3dcee6ed574ea397aed6dfde070827
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2018-01-12 12:57:46 -0500

    MM SDK with Vue.js 2.0 1.0  /datastore_mutation_types.js

--- datastore_mutation_types.js
+++ datastore_mutation_types.js
@@ -1,10 +1,11 @@
 define(function () {
   const types = {
+    SET_API_DETAILS: "SET_API_DETAILS",
+    SET_LOCALE: "SET_LOCALE",
     SET_MALL_DATA: "SET_MALL_DATA",
     SET_META_DATA: "SET_META_DATA",
-    SET_LOCALE: "SET_LOCALE",
-    SET_STORES: "SET_STORES",
-    SET_API_DETAILS: "SET_API_DETAILS"
+    SET_PROPERTY: "SET_PROPERTY",
+    SET_STORES: "SET_STORES"
   }
   return types;
 });
