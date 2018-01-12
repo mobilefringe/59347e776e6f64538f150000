252b8c14602eb03f144171060b33743da9f20f46
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2018-01-12 10:34:06 -0500

    MM SDK with Vue.js 2.0 1.0  /datastore.js

---
+++ datastore.js
@@ -0,0 +1,20 @@
+define(['Vue', 'vuex', 'datastore_getters', 'datastore_actions', 'datastore_mutations'], function (Vue, Vuex, getters, actions, mutations) {
+  Vue.use(Vuex);
+
+  const state = {
+    results: [],
+    meta_data: [],
+    locale: null,
+    url: null
+  }
+
+  const store = new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+  })
+
+  return store;
+    
+});
