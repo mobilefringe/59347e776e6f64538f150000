64841546480825b160c9aa550bde2912a01ab37c
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2018-02-02 14:28:58 -0500

    MM SDK with Vue.js 2.0 1.0  /mapplic.js

--- mapplic.js
+++ mapplic.js
@@ -5,18 +5,8 @@
  */
 
 (function (factory) {
-	if ( typeof define === 'function' && define.amd ) {
-			// AMD. Register as an anonymous module.
-			define(['jquery', 'velocity'], factory);
-	} else if (typeof exports === 'object') {
-			// Node/CommonJS style for Browserify
-			module.exports = factory;
-	} else {
-			// Browser globals
-			factory(jQuery);
-	}
-}(function($, velocity) {
-    window.jQuery = window.$ = $;
+	factory(jQuery);
+}(function($) {
 	"use strict";
 
 	var Mapplic = function(element) {
