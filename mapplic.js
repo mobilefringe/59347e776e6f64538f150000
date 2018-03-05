733ede69f14f8e9756b2a191088fb69ac6857615
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-05 11:22:23 -0500

    MM SDK with Vue.js 2.0 1.0  /mapplic.js

--- mapplic.js
+++ mapplic.js
@@ -775,7 +775,7 @@ define(["jquery", "mousewheel", "hammer"], function(mousewheel, hammer){
 
 										var fill = null;
 										if (location.fill) {fill = location.fill;}
-										else if (self.o.fillcolor) {fill = self.o.fillcolor; console.log("fill", fill);}
+										else if (self.o.fillcolor && self.o.fillcolor !== "none") fill = self.o.fillcolor;
                                         console.log("fill", fill);
 										if (fill) {
 											$(this).css('fill', fill);
