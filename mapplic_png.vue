ecbdae63ec4f4b7fe770cee65570c6f2626a6eb0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-31 10:19:45 -0400

    MM SDK with Vue.js 2.0 1.0  /mapplic_png.vue

--- mapplic_png.vue
+++ mapplic_png.vue
@@ -344,7 +344,7 @@
                         floor_1.locations = [];
                         var stores_on_floors= [];
                         console.log( value.z_coordinate);
-                        if( value.z_coordinate === 0) {
+                        if( value.z_index === 0) {
                             stores_on_floors = vm.storelist;
                         } else {
                             stores_on_floors = _.filter(vm.storelist, function(o) { if(o.z_coordinate == null) {return true;} else { return value.z_index === o.z_coordinate; }});
