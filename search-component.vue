f3e01c13cb3e3f98e3f56feb35d43e581d0ce021
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-07 11:52:50 -0400

    MM SDK with Vue.js 2.0 1.0  /search-component.vue

--- search-component.vue
+++ search-component.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="search-component" v-if="autocomplete">
-    <p class="control has-icon has-icon-right">
-        <label style="display:none;" for="search_box">Search</label>
-      <input id="search_box" type="search" class="input is-large" :placeholder="placeholder" :suggestionAttribute="suggestionAttribute" v-on:input="onInput($event.target.value)"
-        v-on:keyup.esc="isOpen = false" v-on:blur="isOpen = false" v-on:keydown.down="moveDown" v-on:keydown.up="moveUp" v-on:keydown.enter="select"
-        :value="value">
-      <i id="siteSearchBtn" class="fa fa-search" @click="select"></i>
-    </p>
-    <ul v-show="isOpen" class="options-list" v-if="result.length > 0">
-      <li  v-for="(option, index) in result" :class="{'highlighted': index === highlightedPosition}" v-on:mouseenter="highlightedPosition = index" v-on:mousedown="select" :key="index">
-        <slot name="item" :data="option"></slot>
-      </li>
-        
-    </ul>
-    <ul v-else  v-show="isOpen" class="options-list">
-        <li>
-           <p>No Results</p>
-      </li>
-    </ul>
-  </div>
-  <div class="search-component" v-else>
-    <p class="control has-icon has-icon-right">
-        <label style="display:none;" for="search_box">Search</label>
-      <input  id="search_box" type="search" class="input is-large" :placeholder="placeholder" v-on:keydown.enter="select" v-on:input="onInput($event.target.value)"
-        :value="value">
-      <i id="siteSearchBtn" class="fa fa-search"  @click="select"></i>
-    </p>
-  </div>
+	<div class="search-component" v-if="autocomplete">
+		<p class="control has-icon has-icon-right">
+			<label style="display:none;" for="search_box">Search</label>
+			<input id="search_box" type="search" class="input is-large" :placeholder="placeholder" :suggestionAttribute="suggestionAttribute" v-on:input="onInput($event.target.value)"
+				v-on:keyup.esc="isOpen = false" v-on:blur="isOpen = false" v-on:keydown.down="moveDown" v-on:keydown.up="moveUp" v-on:keydown.enter="select"
+				:value="value">
+			<i id="siteSearchBtn" class="fa fa-search" @click="select"></i>
+		</p>
+		<ul v-show="isOpen" class="options-list" v-if="result.length > 0">
+			<li  v-for="(option, index) in result" :class="{'highlighted': index === highlightedPosition}" v-on:mouseenter="highlightedPosition = index" v-on:mousedown="select" :key="index">
+				<slot name="item" :data="option"></slot>
+			</li>
+		</ul>
+		<ul v-else  v-show="isOpen" class="options-list">
+			<li>
+				<p>No Results</p>
+			</li>
+		</ul>
+	</div>
+	<div class="search-component" v-else>
+		<p class="control has-icon has-icon-right">
+			<label style="display:none;" for="search_box">Search</label>
+			<input  id="search_box" type="search" class="input is-large" :placeholder="placeholder" v-on:keydown.enter="select" v-on:input="onInput($event.target.value)"
+				:value="value">
+			<i id="siteSearchBtn" class="fa fa-search"  @click="select"></i>
+		</p>
+	</div>
 </template>
 <!--<template>-->
 <!--  <div class="search-component" v-if="autocomplete">-->
