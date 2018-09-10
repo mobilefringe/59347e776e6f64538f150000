e392cdf349bb1e76ac9176565a8b8777a203298a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-10 10:48:02 -0400

    MM SDK with Vue.js 2.0 1.0  /site-search-component.vue

---
+++ site-search-component.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div>
+        <input id="searchBox" type="text" placeholder="Search" />
+        <i id="siteSearchBtn" class="fa  fa-search"></i>
+        <script type="text/javascript">
+            <!-- console.log("this",  '{{newsletter_email}}') -->
+            var ss360Config = {
+                siteId: '{{siteId}}',
+                searchBox: {selector: '{{searchBoxSelector}}'},
+                suggestions: {
+                    showImages: true,
+                    throttleTime: 150,
+                },
+                style: {
+                    themeColor: '{{themeColor}}'
+                },
+                results: {
+                    embedConfig: {'url':'{{resultPageURL}}','contentBlock':'{{resultPageContainer}}'},
+                    highlightQueryTerms: true
+                }
+            };
+        </script>
+        <script src="https://sitesearch360.com/cdn/sitesearch360-v11.min.js" async></script>
+    </div>
+</template>
