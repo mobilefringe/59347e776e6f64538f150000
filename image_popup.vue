b19654b2fb31f7bd68d9ade00415a001630882cd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-02 11:48:52 -0400

    MM SDK with Vue.js 2.0 1.0  /image_popup.vue

---
+++ image_popup.vue
@@ -0,0 +1,71 @@
+<template>
+    <div v-if="dataLoaded">
+      <div class="popup_container" v-if="currentPopup && show_popup">
+          <i class="fa fa-times close_popup" @click="closePopup()"></i>
+          <a :href="currentPopup.photo_link" target="_target">
+            <img :src="currentPopup.image_url">
+          </a>
+      </div>
+    </div>
+</template>
+<script>
+    define(["Vue", "vuex"], function(Vue, Vuex) {
+        return Vue.component("image-popup-component", {
+            template: template, // the variable template will be injected,
+            data() {
+                return {
+                  show_popup: false,
+                  dataLoaded:false
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;  
+                });
+            },
+            watch: {
+                show_popup() {
+                  if (this.show_popup === true) {
+                    document.body.classList.add("no-scroll");
+                  } else if (this.show_popup === false) {
+                    document.body.classList.remove("no-scroll");
+                  }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone'
+                ]),
+                currentPopup() {
+                  //   var popup = this.$store.state.popups[0];
+                  var popup = null;
+                  if (this.locale == "fr") {
+                    popup = _.find(this.$store.state.popups, function(o) {
+                      return _.includes(o.slug, "french");
+                    });
+                  } else {
+                    popup = _.find(this.$store.state.popups, function(o) {
+                      return _.includes(o.slug, "english");
+                    });
+                  }
+                  return popup;
+                }
+            },
+            methods: {
+                closePopup() {
+                  this.show_popup = false;
+                  document.getElementById("popup_backdrop").style.display = "none";
+                },
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "popups")]);
+                        return results;
+                    } catch(e) {
+                        console.log("Error loading data: " + e.message);    
+                    }
+                },
+            }
+        });
+    });
+</script>
