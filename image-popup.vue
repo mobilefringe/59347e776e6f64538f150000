1c651f23ed53faee4604d8ccde9c1d4ee5db7daf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-02 11:09:42 -0400

    MM SDK with Vue.js 2.0 1.0  /image-popup.vue

--- image-popup.vue
+++ image-popup.vue
@@ -53,21 +53,20 @@
                 }
             },
             computed: {
-                
-    currentPopup() {
-      //   var popup = this.$store.state.popups[0];
-      var popup = null;
-      if (this.locale == "fr") {
-        popup = _.find(this.$store.state.popups, function(o) {
-          return _.includes(o.slug, "french");
-        });
-      } else {
-        popup = _.find(this.$store.state.popups, function(o) {
-          return _.includes(o.slug, "english");
-        });
-      }
-      return popup;
-    },
+                currentPopup() {
+                  //   var popup = this.$store.state.popups[0];
+                  var popup = null;
+                  if (this.locale == "fr") {
+                    popup = _.find(this.$store.state.popups, function(o) {
+                      return _.includes(o.slug, "french");
+                    });
+                  } else {
+                    popup = _.find(this.$store.state.popups, function(o) {
+                      return _.includes(o.slug, "english");
+                    });
+                  }
+                  return popup;
+                },
             }
         });
     });
